9aaaa4dc4b85ec004c83e4d775a540a337270cc5
briandenicola <brian@briandenicola.com>
2023-05-16 09:49:09 -0500

    Parse environmental values

---
+++ scripts/setup-env.sh
@@ -0,0 +1,9 @@
+SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
+INFRA_PATH=$(realpath "${SCRIPT_DIR}/../infrastructure")
+
+export RG=$(terraform -chdir=${INFRA_PATH} output -raw AKS_RESOURCE_GROUP)
+export MSI_NAME=$(terraform -chdir=${INFRA_PATH} output -raw MSI_NAME)
+
+export ACR="bjdcsa.azurecr.io" #existing Azure Container Registry 
+export AKS="gator-28237-aks"   #existing AKS with KEDA extension enabled
+export AKS_RG="gator-28237_rg" #existing AKS RG 
