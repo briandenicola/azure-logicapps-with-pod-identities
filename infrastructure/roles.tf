0a964b6b77471d3f24158a2e5b5089fe49afd8fb
briandenicola <brian@briandenicola.com>
2023-05-16 15:01:16 -0500

    Added additional roles

--- infrastructure/roles.tf
+++ infrastructure/roles.tf
@@ -1,6 +1,20 @@
 resource "azurerm_role_assignment" "blob" {
   scope                = azurerm_storage_account.this.id
-  role_definition_name = "Storage Blob Data Contributor"
+  role_definition_name = "Storage Queue Data Contributor"
+  principal_id         = azurerm_user_assigned_identity.aks_pod_identity.principal_id
+  skip_service_principal_aad_check = true  
+}
+
+resource "azurerm_role_assignment" "queue" {
+  scope                = azurerm_storage_account.this.id
+  role_definition_name = "Storage Account Contributor"
+  principal_id         = azurerm_user_assigned_identity.aks_pod_identity.principal_id
+  skip_service_principal_aad_check = true  
+}
+
+resource "azurerm_role_assignment" "table" {
+  scope                = azurerm_storage_account.this.id
+  role_definition_name = "Storage Blob Data Owner"
   principal_id         = azurerm_user_assigned_identity.aks_pod_identity.principal_id
   skip_service_principal_aad_check = true  
 }
